532a51ae7b4873ca9990007766553e4b4d27b97e
Joey <Joeylai96@hotmail.com>
2021-02-14 18:55:56 +0100

    test

--- swagger_server/service/student_service.py
+++ swagger_server/service/student_service.py
@@ -55,6 +55,10 @@ def delete_student(student_id):
 
 
 def get_student_by_last_name(last_name):
+    student = student_db.get(doc_id=int(1))
+    return student, 200
+
+
     User = Query()
     student = db.get(User.last_name == last_name)
 
